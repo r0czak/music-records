0a33683ba2e91e64d9d7e8f9f1918d2402ccf6b1
r0czak <kaczorb.kontakt@gmail.com>
2023-03-16 19:14:56 +0100

    Initial commit

---
+++ src/main/java/com/ws/musicrecords/MusicRecordsApplication.java
@@ -0,0 +1,13 @@
+package com.ws.musicrecords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicRecordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicRecordsApplication.class, args);
+	}
+
+}
